2da985126fa3709d69f477e43fcf29497a9d0377
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-11-01 22:55:49 +0100

    add functionality to the cart and receiveOrders pages by sending data with POST reqests

--- website/views.py
+++ website/views.py
@@ -9,20 +9,26 @@ import json
 
 views = Blueprint('views', __name__)     #we create a BP named views
 
+selected_pizzas = [] 
+order = []
+
 
 @views.route('/', methods=['GET', 'POST'])  #defining a route 
 @login_required
 def home():
     pizzas = Pizza.query.all()
-    print(pizzas)
-    
     return render_template("home.html",user=current_user, pizzas=pizzas) #send the curr user to home template
 
 
-@views.route('/cart', methods=['GET'])  #cart route
+@views.route('/cart', methods=['GET', 'POST'])  #cart route
 @login_required
 def cart():
-    return render_template("cart.html", user=current_user) 
+    if request.method == 'POST': 
+        pizza_name = request.form.get('pizzaName')
+        selected_pizzas.append(pizza_name)
+        pizzas = Pizza.query.all()
+        return render_template("home.html",user=current_user, pizzas=pizzas) 
+    return render_template("cart.html", user=current_user, pizza = selected_pizzas) 
 
 
 @views.route('/waiting-page', methods=['GET'])  #waitingPage route
@@ -30,7 +36,20 @@ def cart():
 def waitingPage():
     return render_template("waitingPage.html", user=current_user) 
 
-@views.route('/receive-orders', methods=['GET'])  #waitingPage route
+@views.route('/receive-orders', methods=['GET', 'POST'])  #waitingPage route
 @login_required
 def receiveOrders():
-    return render_template("receiveOrders.html", user=current_user) 
+    if request.method == 'POST': 
+        currentOrder = request.form.get('currentOrder')
+        order.append(currentOrder)
+        selected_pizzas = []
+        return render_template("cart.html", user=current_user, pizza = selected_pizzas) 
+    return render_template("receiveOrders.html", user=current_user, order=order) 
+
+@views.route('/remove_pizza', methods=['POST'])
+@login_required
+def remove_pizza():
+    pizza_name = request.form.get('pizzaName')  
+    if pizza_name in selected_pizzas:
+         selected_pizzas.remove(pizza_name)   
+    return render_template("cart.html", user=current_user, pizza = selected_pizzas)    
